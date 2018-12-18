14b7d97dc4e2386555817c1f27466bb572862422
brandonleck <brandevux@gmail.com>
2018-12-17 23:57:55 -0500

    build: Se anade metodos que simulan el turno del robot y evaluan si existe un ganador

--- src/components/ticTacToe/board.jsx
+++ src/components/ticTacToe/board.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { addCheck } from "../../redux/actions/playingAction";
-
+/**
+ * Función encargada de definir que parte del estado global de la aplicación se va a requerir en el componente 'Board'.
+ * @param {Object} state Estado global de la aplicación sobre el cual se desean capturar propiedades.
+ */
 const mapStateToProps = state => ({
   squares: state.squares,
-  xIsNext: state.xIsNext
+  xIsNext: state.xIsNext,
+  completedGame: state.completedGame,
+  winnerFound: state.winnerFound
 });
-
+/**
+ * Función encargada de definir que acciones utilizará el componente 'Board' para modificar el estado global de la aplicación.
+ * @param {Object} dispatch 
+ */
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
@@ -31,7 +39,7 @@ class Board extends React.Component {
   };
 
   render() {
-    const status = `Next player: ${this.props.xIsNext ? "X" : "O"}`;
+    const status = this.props.winnerFound === true ? `The winner is: ${this.props.xIsNext ? 'O' : 'X'}` : `Next player: ${this.props.xIsNext ? "X" : "O"}`;
     return (
       <div>
         <div className="status">{status}</div>
@@ -53,8 +61,37 @@ class Board extends React.Component {
       </div>
     );
   }
+  /**
+   * Método encargado de ejecutar el turno del robot, siempre y cuando el juego no este completo y no exista un ganador.
+   */
+  componentDidUpdate= () => {
+    if (this.props.xIsNext === false && !this.props.completedGame && this.props.winnerFound === false) {
+      this.getRandomShift();
+    }      
+  }
+  /**
+   * Método encargado de realizar una petición a 'random.org' para simular el turno del contrincante.
+   */
+  getRandomShift = () => {
+    let me = this;
+    fetch('https://www.random.org/integers/?num=1&min=0&max=8&col=5&base=10&format=plain&rnd=new').then(function(response) {
+      return response.json();	
+    }).then(function(position) {
+      if (me.props.squares[position] === "") {
+        me.props.addCheck(position);
+      } else {
+        console.log("La posición ya está marcada!");
+        me.getRandomShift();
+      }
+    }).catch(function(error) {
+      console.log(error);
+    })
+  }
 }
-
+/**
+ * Función encargada de construir el componente Square, cada uno de los cuadrados en el interior de Board.
+ * @param {Object} props Propiedades del componente funcional Square
+ */
 const Square = function(props) {
   return (
     <button className="square" onClick={props.changePlayer}>
--- src/redux/reducers/playingReducer.ts
+++ src/redux/reducers/playingReducer.ts
@@ -3,10 +3,12 @@
  */
 const initialState = {
   xIsNext: true,
-  squares: ["", "", "", "", "", "", "", "", ""]
+  squares: ["", "", "", "", "", "", "", "", ""],
+  completedGame: false,
+  winnerFound: false
 };
 /**
- * Reducer encargado de modificar el estado de la aplicación.
+ * Reducer encargado de modificar el estado de la aplicación cada vez que alguien ha realizado un turno.
  * @param state Estado actual de la aplicación
  * @param action Acción que modificará el estado de la aplicación
  */
@@ -14,13 +16,44 @@ export const playingReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case "CHECK":
       const squares = state.squares.slice();
+      let completedGame = null,
+        winnerFound = null;
+
       squares[action.payload.position] = state.xIsNext ? "X" : "O";
+      completedGame = squares.indexOf("") === -1 ? true : false;
+      winnerFound = calculateWinner(squares) === null? false : true;
+
       return {
         ...state,
         xIsNext: !state.xIsNext,
-        squares
+        squares,
+        completedGame,
+        winnerFound
       };
     default:
       return state;
   }
-};
+}
+/**
+ * Función encargada de evaluar si en el juego ya existe un ganador.
+ * @param squares Estado de los cuadrados del tablero
+ */
+const calculateWinner = (squares: any) =>  {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
